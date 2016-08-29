a56f91e09d034e21133e64dfaab1f5f338a2f308
JOONGSEOD <joonseoD@gmail.com>
2016-08-29 13:00:47 +0900

    중서_160829_1:00PM_중서게시판세팅

--- practice01/src/main/java/vfront/github/pr01/js/JsController.java
+++ practice01/src/main/java/vfront/github/pr01/js/JsController.java
@@ -2,6 +2,7 @@ package vfront.github.pr01.js;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/js")
@@ -12,11 +13,22 @@ public class JsController {
 	public ModelAndView front(){
 		System.out.println("js's controller");
 		
+		ModelAndView mav = new ModelAndView("/js/boardHome");
 		
-		// 본인 작업함!!!
 		
-		//수정2222
+		return mav;
+	}
+	
+	
+	@RequestMapping("/joinForm")
+	public ModelAndView joinForm(){
 		
+		return new ModelAndView("/js/tryJoin");
+	}
+	
+	@RequestMapping("/idConf")
+	@ResponseBody
+	public String idConf(){
 		return null;
 	}
 	
